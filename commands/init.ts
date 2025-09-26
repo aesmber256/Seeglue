280f7c9ec762d3e9a1174af3a1f9a04d7f209932
A(ES)mber-256 <a.es.mber.256@proton.me>
2025-09-27 01:07:14 +0200

    Slight updates

--- commands/init.ts
+++ commands/init.ts
@@ -6,7 +6,7 @@ import { ensureDir } from "@std/fs/ensure-dir";
 import { join } from "@std/path/join";
 
 import gitignore from "../resource/gitignore.txt" with { type: "text" };
-import lib from "../resource/lib.d.ts" with { type: "text" };
+import lib from "../resource/seeglue.d.ts" with { type: "text" };
 import buildFile from "../resource/build.txt" with { type: "text" };
 
 export default async function(args: CliArgs) {
@@ -17,7 +17,7 @@ export default async function(args: CliArgs) {
     await ensureDir(tree.metaRoot);
     await Promise.all([
         Deno.writeTextFile(join(tree.metaRoot, ".gitignore"), gitignore),
-        Deno.writeTextFile(join(tree.metaRoot, "build.d.ts"), lib),
+        Deno.writeTextFile(join(tree.metaRoot, "seeglue.d.ts"), lib),
         Deno.writeTextFile(tree.buildFile, buildFile)
     ]);
 }
